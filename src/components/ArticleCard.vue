956dd98f1796d244e3c7213a70b1a4cdec36bf3d
Candice <2751564851@qq.com>
2025-05-07 03:05:02 +0800

    D

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="article-card">
+      <!-- 卡片内容 -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['article']
+  }
+  </script>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="pagination">
+      <!-- 分页组件 -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['currentPage', 'totalPages']
+  }
+  </script>
